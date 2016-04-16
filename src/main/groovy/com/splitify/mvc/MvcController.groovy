535841350c342e602d4f980f90f012378a387fb8
Alejandro Romero <alexromero88@gmail.com>
2016-04-16 14:15:35 +0100

    (Example) Controller redirecting to a view

--- src/main/groovy/com/splitify/mvc/MvcController.groovy
+++ src/main/groovy/com/splitify/mvc/MvcController.groovy
@@ -1,7 +1,9 @@
 package com.splitify.mvc
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
@@ -9,5 +11,11 @@ class MvcController {
     @RequestMapping(value = "/")
     def @ResponseBody hello() {
         return "Hello World!"
+    }
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greetingView";
     }
 }
