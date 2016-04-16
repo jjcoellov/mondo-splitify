eeaf808fc069ebfef238596112e5cdab92240f6c
Juanjo Coello <juanjo@jjcoellov.es>
2016-04-16 17:40:14 +0100

    use mondo client when splitting amount

--- src/main/groovy/com/splitify/mvc/split/SplitService.groovy
+++ src/main/groovy/com/splitify/mvc/split/SplitService.groovy
@@ -1,9 +1,8 @@
 package com.splitify.mvc.split
 
+import com.splitify.mvc.client.MondoAPIClient
 import com.splitify.mvc.friends.Friend
 import com.splitify.mvc.friends.FriendsRepository
-import com.splitify.mvc.mondo.ApiConfig
-import groovyx.net.http.RESTClient
 import org.apache.log4j.LogManager
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,12 +13,14 @@ class SplitService {
 
     private static final Logger logger = LogManager.getLogger(SplitService);
 
+    static final String ACC_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaSI6Im9hdXRoY2xpZW50XzAwMDA5NFB2SU5ER3pUM2s2dHo4anAiLCJleHAiOjE0NjA5ODEyMTcsImlhdCI6MTQ2MDgwODQxNywianRpIjoidG9rXzAwMDA5N0Z2dlhXdWxMR3NVczlqdFoiLCJ1aSI6InVzZXJfMDAwMDk3RnZidXdNMHZEUEVwR09PWCIsInYiOiI0In0.DlWlJ3TsRNz8TVFjpn2H653d9N4ZXgthd86GfcyckUI"
+
     @Autowired
     FriendsRepository friendRepository
 
     def split(SplitRequest splitRequest) {
 
-        Integer amount = retrieveTransactionAmount(ApiConfig.ACCESS_TOKEN, splitRequest.transactionId)
+        Integer amount = retrieveTransactionAmount(ACC_TOKEN, splitRequest.transactionId)
 
         List<Friend> friendsToSplit = friendRepository.retrieveFriends(splitRequest.friends)
 
@@ -33,12 +34,9 @@ class SplitService {
         // persist split operation
     }
 
-    private Integer retrieveTransactionAmount(String authToken, String transactionId) {
+    private static Integer retrieveTransactionAmount(String accessToken, String transactionId) {
 
-        def mondoClient = new RESTClient(ApiConfig.URL)
-        mondoClient.defaultRequestHeaders['Authorization'] = "Bearer $authToken"
-
-        def response = mondoClient.get(
+        def response = new MondoAPIClient(accessToken).get(
                 path: "/transactions/$transactionId",
         )
 
