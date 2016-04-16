de97db45d046e49dd1d1b7c98439d462cc9930d6
Juan Coello <juan_coello@intuit.com>
2016-04-16 12:59:10 +0100

    commit hello world in spring boot

---
+++ src/main/groovy/com/splitify/mvc/MvcController.groovy
@@ -0,0 +1,13 @@
+package com.splitify.mvc
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class MvcController {
+    @RequestMapping(value = "/")
+    def @ResponseBody hello() {
+        return "Hello World!"
+    }
+}
