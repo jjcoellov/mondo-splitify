310d0d996b7b5590481544ee9c666d9f20a381d6
Juan Coello <juan_coello@intuit.com>
2016-04-16 14:53:53 +0100

    Parse webhook notification

---
+++ src/main/groovy/com/splitify/mvc/webhook/WebhookEvent.groovy
@@ -0,0 +1,34 @@
+package com.splitify.mvc.webhook
+
+import groovy.json.JsonSlurper
+import groovy.transform.Canonical
+
+@Canonical
+class WebhookEvent {
+
+    String accountId
+
+    String description
+
+    String merchantName
+
+    String merchantAddress
+
+    BigInteger amount
+
+    String currency
+
+    static WebhookEvent parse(String payload) {
+        def JsonSlurper jsonSlurper = new JsonSlurper()
+        def parsedNotification = jsonSlurper.parseText(payload)
+        return new WebhookEvent(
+                accountId: parsedNotification.data.account_id,
+                description: parsedNotification.data.description,
+                merchantName: parsedNotification.data.merchant.name,
+                merchantAddress: parsedNotification.data.merchant.address.short_formatted,
+                amount: parsedNotification.data.amount,
+                currency: parsedNotification.data.currency
+        )
+    }
+
+}
