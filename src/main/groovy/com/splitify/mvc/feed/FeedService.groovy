f15c949926daa34ce2bc62070eae2e6f3a9708d1
Barroso, Juan <Juan_Barroso@intuit.com>
2016-04-16 16:59:59 +0100

    allowing access token on the feedService

--- src/main/groovy/com/splitify/mvc/feed/FeedService.groovy
+++ src/main/groovy/com/splitify/mvc/feed/FeedService.groovy
@@ -8,18 +8,16 @@ import static groovyx.net.http.ContentType.URLENC
 class FeedService {
 
 
-
-    void createDummyFeed(String accountId) {
+    void createDummyFeed(String accountId, String accessToken = null) {
         sendFeed(accountId,"test","http://google.com","http://www.nyan.cat/cats/original.gif")
     }
 
-    private sendFeed(String accountId,String title, String URL, String imageURL) {
+    private sendFeed(String accountId,String title, String URL, String imageURL, String accessToken = null) {
 
-        def mondoClient = new RESTClient("https://staging-api.gmon.io/")
-        mondoClient.defaultRequestHeaders['Authorization'] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaSI6Im9hdXRoY2xpZW50XzAwMDA5NFB2SU5ER3pUM2s2dHo4anAiLCJleHAiOjE0NjA5Nzg2ODksImlhdCI6MTQ2MDgwNTg4OSwianRpIjoidG9rXzAwMDA5N0ZzQVFyUFJTR1RkYk11Q3YiLCJ1aSI6InVzZXJfMDAwMDk3RnJHS1ZWc01ad0xVOXg0YiIsInYiOiI0In0.2-x9BJLQ-nqEX_I3K4rptVCimYbbKGCTpXxxMblpk4Y"
+        def mondoClient = getFeedClient(accessToken)
 
         def response = mondoClient.post(
-                path:"/feed",
+                //path:"/feed",
                 body: [ "account_id": "$accountId",
                         "type": "basic",
                         "url": "$URL",
@@ -29,6 +27,16 @@ class FeedService {
                 ],
                 requestContentType: URLENC
         )
+    }
+
+    RESTClient getFeedClient(String accessToken) {
+        def mondoClient = new RESTClient("https://staging-api.gmon.io/feed")
+        if (!accessToken) {
+            accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaSI6Im9hdXRoY2xpZW50XzAwMDA5NFB2SU5ER3pUM2s2dHo4anAiLCJleHAiOjE0NjA5Nzg2ODksImlhdCI6MTQ2MDgwNTg4OSwianRpIjoidG9rXzAwMDA5N0ZzQVFyUFJTR1RkYk11Q3YiLCJ1aSI6InVzZXJfMDAwMDk3RnJHS1ZWc01ad0xVOXg0YiIsInYiOiI0In0.2-x9BJLQ-nqEX_I3K4rptVCimYbbKGCTpXxxMblpk4Y"
+        }
+        mondoClient.defaultRequestHeaders['Authorization'] = "Bearer $accessToken"
+        return mondoClient
+
     }
 
 }
