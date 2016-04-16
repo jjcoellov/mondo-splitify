65dd45bfaa33601b63a708116167d434c0e452e8
Barroso, Juan <Juan_Barroso@intuit.com>
2016-04-16 14:42:03 +0100

    empty feedService

---
+++ src/main/groovy/com/splitify/service/FeedService.groovy
@@ -0,0 +1,15 @@
+package com.splitify.service
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+class FeedService {
+
+    void createDummyFeed() {
+
+
+
+    }
+
+}
