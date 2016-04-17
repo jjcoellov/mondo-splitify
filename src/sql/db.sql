a16c51450cf0b71cadf51c2f2e3b437ce7f3b96d
Esau Rodriguez <esau.rodriguez@octopusinvestments.com>
2016-04-17 13:24:50 +0100

    New sql version

--- src/sql/db.sql
+++ src/sql/db.sql
@@ -1,5 +1,21 @@
-CREATE TABLE SplitRequest (
+CREATE TABLE split_request (
   split_request_id serial PRIMARY KEY,
-  transaction_id varchar(200) not null,
-  account_id varchar(200) not null,
-  friends_list varchar(2000) not null);
+  transaction_id VARCHAR(200) not null,
+  account_id VARCHAR(200) not null,
+  amount BIGINT not null
+);
+
+CREATE TABLE friend (
+  friend_id serial PRIMARY KEY,
+  name VARCHAR(200) not null,
+  account_id VARCHAR(200) not null,
+  access_token VARCHAR(1000) not null,
+  phone_number VARCHAR(100)
+);
+
+CREATE TABLE split_request_friend (
+  split_request_id BIGINT NOT NULL ,
+  friend_id BIGINT NOT NULL,
+  amount BIGINT not null,
+  status INT default 0
+);
