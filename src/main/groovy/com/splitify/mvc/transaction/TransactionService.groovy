ecd5d073c6dde03574427718d91e0cb5e2ba0fb7
Juanjo Coello <juanjo@jjcoellov.es>
2016-04-17 10:50:56 +0100

    Push (for real) Transaction Service

---
+++ src/main/groovy/com/splitify/mvc/transaction/TransactionService.groovy
@@ -0,0 +1,19 @@
+package com.splitify.mvc.transaction
+
+import com.splitify.mvc.client.MondoAPIClient
+import org.springframework.stereotype.Service
+
+@Service
+class TransactionService {
+
+
+    Integer retrieveTransactionAmount(String accessToken, String transactionId) {
+
+        def response = new MondoAPIClient(accessToken).get(
+                path: "/transactions/$transactionId",
+        )
+
+        return response.responseData.transaction.amount ?: 0
+    }
+
+}
